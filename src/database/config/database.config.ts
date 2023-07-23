b9de0b4cfa99ab3eb47f9cebbd5b921b784dec11
Igor Matos <sarkan.igor@gmail.com>
2023-07-23 00:25:19 -0300

    criacao de migration com generate

--- src/database/config/database.config.ts
+++ src/database/config/database.config.ts
@@ -1,8 +1,17 @@
 import { DataSource } from "typeorm";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const dataSource = new DataSource({
   type: "postgres",
+  port: 3333,
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
   url: process.env.DB_URL,
+
   ssl: {
     rejectUnauthorized: false,
   },
